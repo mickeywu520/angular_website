b3d628f1171e2d46fcc3c25363ac74c7397e9730
mickey <mickeywu520@gmail.com>
2024-07-14 19:54:39 +0800

    swiper function

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, AfterViewInit } from '@angular/core';
+import { register } from 'swiper/element/bundle';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class HomeComponent {
-
+export class HomeComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    register();
+  }
 }
