a10e231c17a5c6aa24bae4186eb1b0a705ed9b05
mickey <mickeywu520@gmail.com>
2024-07-15 00:14:06 +0800

    update content onto swiper image

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,18 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA, AfterViewInit } from '@angular/core';
 import { register } from 'swiper/element/bundle';
+import {
+  animate,
+  group,
+  query,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import { bootstrapApplication } from '@angular/platform-browser';
+import {
+  BrowserAnimationsModule,
+  provideAnimations,
+} from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-home',
